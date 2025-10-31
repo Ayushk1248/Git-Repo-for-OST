b9d6fcac13206109793e27d956fd465295a5aa85
Ayushk1248 <ayushk0801@gmail.com>
2025-10-31 09:15:59 +0530

    Add files via upload

---
+++ multmatrix.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int a[10][10],b[10][10],c[10][10],i,j,m,n,l,k;
+    scanf("%d%d%d",&m,&l,&n);
+    for(i=1;i<=m;i++)
+        for(j=1;j<=l;j++)
+        scanf("%d",&a[i][j]);
+    for(i=1;i<=l;i++)
+        for(j=1;j<=n;j++)
+        scanf("%d",&b[i][j]);
+    for(i=1;i<=m;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            c[i][j]=0;
+            for(k=1;k<=l;k++)
+                c[i][j]=c[i][j]+a[i][k]*b[k][j];
+
+        }
+    }
+    for(i=1;i<=m;i++)
+    {
+        printf("\n\n");
+        for(j=1;j<=n;j++)
+            printf("%4d",c[i][j]);
+    }
+}
---
+++ summatrix.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,m,a[10][10],sum,gsum;
+    scanf("%d%d",&m,&n);
+    for(i=1;i<=m;i++)
+        for(j=1;j<=n;j++)
+        scanf("%d",&a[i][j]);
+    for(i=1;i<=m;i++)
+    {
+        sum=0;
+        printf("\n\n");
+        for(j=1;j<=n;j++)
+        {
+            printf("%4d",a[i][j]);
+            sum=sum+a[i][j];
+        }
+        printf("%10d",sum);
+    }
+    printf("\n\n");
+    gsum=0;
+    for(j=1;j<=n;j++)
+    {
+      sum=0;
+    for(i=1;i<=m;i++)
+    {
+        sum=sum+sum+a[i][j];
+    }
+    printf("%4d",sum);
+    gsum=gsum+sum;
+}
+printf("%10d",gsum);
+}
